b2ab328df2c58e5dd82e94446e0995e80b86b010
namu627 <0627eric@naver.com>
2024-09-23 22:58:10 +0900

    2805

--- 2805.py
+++ 2805.py
@@ -1,4 +1,19 @@
-n,m=input().split()
-a=input().split()
+import sys
+n,m=map(int,sys.stdin.readline().split())
+a=list(map(int, sys.stdin.readline().split()))
 a.sort()
-print(a)
+start=1
+end=a[-1]
+ans=0
+while start<=end:
+    mid=(start+end)//2
+    tree=0
+    for i in a:
+        if mid<i:
+            tree=tree+(i-mid)
+    if tree<m:
+        end=mid-1
+    else:
+        ans=mid
+        start=mid+1
+print(ans)
