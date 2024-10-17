edb6067816163150db5a9e14401c5115147da112
namu627 <0627eric@naver.com>
2024-10-18 00:56:49 +0900

    11651

--- 11651.py
+++ 11651.py
@@ -2,6 +2,6 @@ n=int(input())
 arr=[]
 for _ in range(n):
     arr.append(list(map(int, input().split())))
-arr.sort(key=lambda x: (x[1], x[0])) #두번째 값이 같을경우 첫번째 값을 기준으로 오름차순
+arr.sort(key=lambda x: (x[1], x[0])) #sort 함수 설정을 저렇게 하면 두번째 값이 같을경우 첫번째 값을 기준으로 오름차순 정렬함
 for i in arr:
     print(*i)
