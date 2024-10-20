0a0f2d12e89c67ad41fed6d75b961583dd8e8b90
namu627 <0627eric@naver.com>
2024-10-20 15:53:12 +0900

    2110

--- 18870.py
+++ 18870.py
@@ -1,14 +1,14 @@
 import sys
 n=int(sys.stdin.readline())
-arr=list(map(int, sys.stdin.readline().split()))
-index=sorted(set(arr))
+arr=list(map(int, sys.stdin.readline().split())) #2 4 -10 4 -9
+arr2=sorted(set(arr)) #중복제거하고 오름차순 정렬해서 리스트만듦. -10 -9 2 4 
 # 시간초과로 틀림
-# for i in range(len(index)):
+# for i in range(len(arr2)):
 #     for j in range(n):
-#         if arr[j]==index[i]:
+#         if arr[j]==arr2[i]:
 #             arr[j]=i
 dic={} #2중for문 쓰면 시간초과 나와서 딕셔너리 쓰기로 함
-for i in range(len(index)):
-    dic[index[i]]=i
+for i in range(len(arr2)): #i=0~3
+    dic[arr2[i]]=i #{-10: 0, -9: 1, 2: 2, 4: 3}
 for i in arr:
-    print(dic[i], end=' ')
+    print(dic[i], end=' ') #arr 값 돌면서 딕셔너리에 저장된 key값으로 value값 출력하기
--- 2110.py
+++ 2110.py
@@ -1,7 +1,28 @@
-h,m=map(int, input().split())
-if h==0:
-    h+=24
-if m<45:
-    h-=1
-    m+=60
-print(h,m-45)
+import sys
+input=sys.stdin.readline
+
+n,c=map(int, input().split())
+arr=[]
+for _ in range(n):
+    arr.append(int(input()))
+arr.sort()
+
+start=1
+end=arr[-1]-arr[0]
+ans=0 #답; 최대 공유기 거리
+
+while start<=end: #start값이 end값보다 커지면 종료
+    mid=(start+end)//2 #현재 공유기 거리
+    home=arr[0] #현재 집. 한개는 무조건 처음 집에 설치할것
+    wifi=1 #공유기 설치한 개수
+    for i in range(1, n):
+        if arr[i]>=home+mid: #현재 집에서 설정한 공유기 거리만큼 더했을때 그 이상의 거리에 있는 집이 있으면 공유기 하나 더 설치
+            home=arr[i] #공유기 설치했으니까 현재 집 옮겨주고
+            wifi=wifi+1 #공유기 설치했으니까 설치한 개수 하나 더해주고
+    if wifi>=c:
+        start=mid+1
+        ans=wifi
+    else:
+        end=mid-1
+print(ans)
+        
