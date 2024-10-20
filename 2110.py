ac767b890664de53335d447bd6010d6521e3c439
namu627 <0627eric@naver.com>
2024-10-20 16:55:05 +0900

    16564

--- 2110.py
+++ 2110.py
@@ -7,8 +7,8 @@ for _ in range(n):
     arr.append(int(input()))
 arr.sort()
 
-start=1
-end=arr[-1]-arr[0]
+start=1 #집이 최소 두개고 같을수 없으니 최소 거리는 1
+end=arr[-1]-arr[0] #초기값은 첫집과 마지막집 사이의 거리
 ans=0 #답; 최대 공유기 거리
 
 while start<=end: #start값이 end값보다 커지면 종료
@@ -19,10 +19,9 @@ while start<=end: #start값이 end값보다 커지면 종료
         if arr[i]>=home+mid: #현재 집에서 설정한 공유기 거리만큼 더했을때 그 이상의 거리에 있는 집이 있으면 공유기 하나 더 설치
             home=arr[i] #공유기 설치했으니까 현재 집 옮겨주고
             wifi=wifi+1 #공유기 설치했으니까 설치한 개수 하나 더해주고
-    if wifi>=c:
-        start=mid+1
-        ans=wifi
-    else:
-        end=mid-1
+    if wifi>=c: #설치한 공유기 수가 원하는 공유기 수와 같거나 그거보다 크면
+        start=mid+1 #start값을 mid+1값으로 바꿔서 재탐색; 다른 값이 나올수도 있으니
+        ans=mid #현재 공유기 거리를 답에 저장
+    else: #설치한 공유기 수가 원하는 공유기 수보다 작으면
+        end=mid-1 #거리를 좁혀서 재탐색
 print(ans)
-        
