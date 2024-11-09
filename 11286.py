399c1f48f8b341106b9b759f04aea46db05fdb44
namu627 <0627eric@naver.com>
2024-11-09 15:31:29 +0900

    2908

--- 11286.py
+++ 11286.py
@@ -1,6 +1,7 @@
-apb=[-1]*26 #알파벳 26글자
-s=list(input())
-for i in range(len(s)):
-    if apb[ord(s[i])-97]==-1:
-        apb[ord(s[i])-97]=i
-print(apb)
+a,b=input().split()
+a=int(a[::-1])
+b=int(b[::-1])
+if a>b:
+    print(a)
+else:
+    print(b)
