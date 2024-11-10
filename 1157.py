de34997a588a5fc999a7f25371fd1b20fe6f1b85
namu627 <0627eric@naver.com>
2024-11-10 16:04:07 +0900

    1157

--- 1157.py
+++ 1157.py
@@ -1,4 +1,9 @@
-word=input().upper() #Mississipi
-wordlist=list(set(word)) #['S', 'M', 'I', 'P']
-
-print(wordlist)
+word=input().upper() #mississipi -> MISSISSIPI
+wordlist=list(set(word)) #['S', 'P', 'M', 'I']
+arr=[]
+for i in wordlist:
+    arr.append(word.count(i)) #wordlist에 저장된 알파벳 하나씩 비교하면서 개수세기
+if arr.count(max(arr))>1: #제일 많이 나온 알파벳의 갯수가 2개 이상이면
+    print("?")
+else:
+    print(wordlist[arr.index(max(arr))]) #제일 많이나온 알파벳의 인덱스를 통해 wordlist에서 찾아 출력하기
---
+++ 25206.py
@@ -0,0 +1,6 @@
+score=0
+for i in range(20):
+    a,b,c=input().split()
+    b=float(b)
+    score+=b
+print(score/20)
