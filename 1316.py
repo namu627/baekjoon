9c3e8ea6b7bd9ffad8ac97d4bde534bb744deb84
namu627 <0627eric@naver.com>
2024-11-11 23:55:53 +0900

    1316

---
+++ 1316.py
@@ -0,0 +1,11 @@
+n=int(input())
+ans=n
+for _ in range(n):
+    word=input()
+    for i in range(len(word)-1):
+        if word[i]==word[i+1]:
+            continue
+        elif word[i] in word[i+1:]:
+            ans-=-1
+            break
+print(ans)
