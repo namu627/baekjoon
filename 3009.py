427a74af523bb767e021cbe4f57ec7edaf79a962
namu627 <0627eric@naver.com>
2024-12-12 22:05:36 +0900

    10101

--- 3009.py
+++ 3009.py
@@ -1,10 +1,11 @@
-x=[]
-y=[]
-for _ in range(3):
+#마지막 좌표는 홀수번 나온 숫자들 조합이므로
+x=[] #x좌표만 따로넣고
+y=[] #y좌표만 따로넣어서 계산
+for _ in range(3): 
     a,b=map(int,input().split())
-    if a in x: #x좌표
+    if a in x: #x좌표 리스트에 똑같은 값이 있으면 삭제
         x.remove(a)
-    else:
+    else: #아니면 좌표 추가
         x.append(a)
     if b in y:
         y.remove(b)
