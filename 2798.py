91edaff16da81591b30f3edf5e9e1a0976fabd11
namu627 <0627eric@naver.com>
2024-12-15 18:12:02 +0900

    19532

--- 2798.py
+++ 2798.py
@@ -1,8 +1,8 @@
-from itertools import permutations
+from itertools import permutations #순열과 조합을 쓰기위한 임포트
 n,m=map(int,input().split())
 card=list(map(int,input().split())) #n장의 카드들
 ans=[]
-for i in permutations(card,3):
-    if sum(i)<=m:
+for i in permutations(card,3): #n장의 카드들중에 m개의 카드를 뽑아서 조합하고
+    if sum(i)<=m: #그 합이 m보다 작거나 같으면 ans에 저장
         ans.append(sum(i))
-print(max(ans))
+print(max(ans)) #그리고 저장된 값중 가장 큰값을 답으로 출력
