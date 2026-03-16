d85e33c2d252aaaf976d61439ab5b6e6c813ec7a
namu627 <0627eric@naver.com>
2026-03-16 15:36:20 +0900

    24723.py

--- 1764.py
+++ 1764.py
@@ -1,10 +1,4 @@
 n,m=map(int,input().split())
-<<<<<<< HEAD
-n_list=[]
-for i in range(n):
-    n_list.append(input())
-
-=======
 듣=set(input() for _ in range(n))
 보=set(input() for _ in range(m))
 듣보=[]
@@ -15,4 +9,3 @@ for i in 듣: #듣지도 못한 사람중에
 print(len(듣보))
 for i in sorted(듣보): #사전순으로 정렬해서 출력
     print(i)
->>>>>>> 9175fd09614d6b5d4502b02aeaef7f7443859c3f
