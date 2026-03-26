99041642f7d55ff0187bd11d2c40a71d81630c14
namu627 <0627eric@naver.com>
2026-03-26 23:06:13 +0900

    24264

---
+++ 24264.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             sum <- sum + A[i] × A[j]; # 코드1
+#     return sum;
+# }
+
+print(n*n)
+print(2)
