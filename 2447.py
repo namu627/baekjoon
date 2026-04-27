2cb7ad7006561ea238fa185e22674fa803d9b605
namu627 <0627eric@naver.com>
2026-04-27 21:46:12 +0900

    2447

---
+++ 2447.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+
+def draw_stars(n):
+    if n==1:
+        return ['*']
+
+    stars=draw_stars(n//3)
+    l=[]
+
+    for s in stars:
+        l.append(s*3)
+    for s in stars:
+        l.append(s+' '*(n//3)+s)
+    for s in stars:
+        l.append(s*3)
+    return l
+
+n=int(input())
+print('\n'.join(draw_stars(n)))
